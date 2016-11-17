b214f24fa9f49c2c4825f5e8ce8453aef0eeee06
kimdah3 <dahlberg.kim20@gmail.com>
2016-11-17 16:11:00 +0100

    Flyttat node_modules-filer med gulp

--- src/GameOfStocksHT16/wwwroot/js/systemjs.config.js
+++ src/GameOfStocksHT16/wwwroot/js/systemjs.config.js
@@ -6,7 +6,7 @@
     System.config({
         paths: {
             // paths serve as alias
-            'npm:': 'node_modules/'
+            'npm:': 'lib/nodemodules/'
         },
         // map tells the System loader where to look for things
         map: {
