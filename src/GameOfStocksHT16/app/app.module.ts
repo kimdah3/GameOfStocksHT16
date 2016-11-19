cdc131343d0bb7bd0d3d0ba8d489828855b2fd05
johnsrsly <johnelarssonz@gmail.com>
2016-11-19 15:22:34 +0100

    fixat routing, alltså navigering, med angular. 50/50 work?

--- src/GameOfStocksHT16/app/app.module.ts
+++ src/GameOfStocksHT16/app/app.module.ts
@@ -1,9 +1,18 @@
 ﻿import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+
+import { routing } from "./app.routing";
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from "./welcome/welcome.component";
+
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        routing
+    ],
+    declarations: [AppComponent, WelcomeComponent],
     bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/GameOfStocksHT16/wwwroot/app/app.component.js
+++ src/GameOfStocksHT16/wwwroot/app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'gos-app',
-            templateUrl: 'app/htmlTemplates/app/app.component.html'
+            templateUrl: 'app/html/app.component.html'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
