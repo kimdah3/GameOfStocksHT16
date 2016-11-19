95f477fcf949c61a753a001da6333a55e665ccf2
johnsrsly <johnelarssonz@gmail.com>
2016-11-20 00:46:20 +0100

    Revert "fixat routing, alltså navigering, med angular. 50/50 work?"
    This reverts commit cdc131343d0bb7bd0d3d0ba8d489828855b2fd05.

--- src/GameOfStocksHT16/app/app.module.ts
+++ src/GameOfStocksHT16/app/app.module.ts
@@ -1,18 +1,9 @@
 ﻿import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { routing } from "./app.routing";
-
-import { AppComponent } from './app.component';
-import { WelcomeComponent } from "./welcome/welcome.component";
-
+import { AppComponent }   from './app.component';
 @NgModule({
-    imports: [
-        BrowserModule,
-        routing
-    ],
-    declarations: [AppComponent, WelcomeComponent],
+    imports: [BrowserModule],
+    declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
-
 export class AppModule { }
--- src/GameOfStocksHT16/wwwroot/app/app.module.js
+++ src/GameOfStocksHT16/wwwroot/app/app.module.js
@@ -10,19 +10,14 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
-var app_routing_1 = require("./app.routing");
 var app_component_1 = require('./app.component');
-var welcome_component_1 = require("./welcome/welcome.component");
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [
-                platform_browser_1.BrowserModule,
-                app_routing_1.routing
-            ],
-            declarations: [app_component_1.AppComponent, welcome_component_1.WelcomeComponent],
+            imports: [platform_browser_1.BrowserModule],
+            declarations: [app_component_1.AppComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
         __metadata('design:paramtypes', [])
