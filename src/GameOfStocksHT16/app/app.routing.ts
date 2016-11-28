f57931e27415d941c75619e5e9db9060d4028ad3
johnsrsly <johnelarssonz@gmail.com>
2016-11-28 15:43:57 +0100

    Börjat med profilsida.

--- src/GameOfStocksHT16/app/app.routing.ts
+++ src/GameOfStocksHT16/app/app.routing.ts
@@ -3,12 +3,14 @@
 import { WelcomeComponent } from "./welcome/welcome.component";
 import { StockListComponent } from "./Stocks/stock-list.component";
 import { StockComponent } from "./Stocks/stock.component";
+import { ProfileComponent } from "./Profile/profile.component";
 
 const appRoutes: Routes = [
     { path: "", redirectTo: "welcome", pathMatch: "full" },
     { path: "welcome", component: WelcomeComponent },
     { path: "stocks", component: StockListComponent },
-    { path: "stock/:id", component: StockComponent }
+    { path: "stock/:id", component: StockComponent },
+    { path: "profile", component: ProfileComponent }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
--- src/GameOfStocksHT16/wwwroot/app/app.routing.js
+++ src/GameOfStocksHT16/wwwroot/app/app.routing.js
@@ -3,11 +3,13 @@ var router_1 = require("@angular/router");
 var welcome_component_1 = require("./welcome/welcome.component");
 var stock_list_component_1 = require("./Stocks/stock-list.component");
 var stock_component_1 = require("./Stocks/stock.component");
+var profile_component_1 = require("./Profile/profile.component");
 var appRoutes = [
     { path: "", redirectTo: "welcome", pathMatch: "full" },
     { path: "welcome", component: welcome_component_1.WelcomeComponent },
     { path: "stocks", component: stock_list_component_1.StockListComponent },
-    { path: "stock/:id", component: stock_component_1.StockComponent }
+    { path: "stock/:id", component: stock_component_1.StockComponent },
+    { path: "profile", component: profile_component_1.ProfileComponent }
 ];
 exports.routing = router_1.RouterModule.forRoot(appRoutes);
 //# sourceMappingURL=app.routing.js.map
